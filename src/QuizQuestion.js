faa0dc79f82deb0a27f4fe62807f51054f287343
srmohan <sridhar.mohan.ext@nokia.com>
2019-10-24 14:20:13 +0530

    Completed module

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -10,7 +10,7 @@ class QuizQuestion extends Component {
         </section>
         <section className="buttons">
           <ul>
-            {this.props.quiz_question.answer_options.map((index,answer_option) => {
+            {this.props.quiz_question.answer_options.map((answer_option,index) => {
              return <QuizQuestionButton key={index} button_text={answer_option} />
             })}
           </ul>
